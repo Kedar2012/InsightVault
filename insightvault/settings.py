665433ddc017ea140e315c511a21a560eed4e4f9
Kedar Damkondwar <kedardamkondwar@gmail.com>
2026-05-01 01:42:11 +0530

    Added Swagger API page in UI

--- insightvault/settings.py
+++ insightvault/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'fraudlog',
     'accounts.apps.AccountsConfig',
     'rest_framework',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- insightvault/urls.py
+++ insightvault/urls.py
@@ -3,14 +3,32 @@ from django.urls import path, include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="InsightVault API",
+        default_version="v1",
+        description="API documentation for InsightVault",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="support@insightvault.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', views.Home, name='home'),
 
-    path('accounts/', include('django.contrib.auth.urls')),
-
     path('api/accounts/', include('accounts.urls')),
     path('api/transactions/', include('transactions.urls')),
     path('api/fraudlog/', include('fraudlog.urls')),
+    
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
