fdac9b18390bcfd9ed50449881eafe9ba8b52df3
Kedar Damkondwar <kedardamkondwar@gmail.com>
2026-05-05 23:33:10 +0530

    Add CI pipeline for pytest + pylint

--- insightvault/settings.py
+++ insightvault/settings.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY", "dummy-secret")
 
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
@@ -64,20 +64,20 @@ DATABASES = {
     # }
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT"),
+        'NAME': os.getenv("DB_NAME", "testdb"),
+        'USER': os.getenv("DB_USER", "testuser"),
+        'PASSWORD': os.getenv("DB_PASSWORD", "testpass"),
+        'HOST': os.getenv("DB_HOST", "localhost"),
+        'PORT': os.getenv("DB_PORT", "5432"),
     }
 }
 
 MONGO_DB = {
-    "NAME": os.getenv("MDB_NAME"),
-    "HOST": os.getenv("MDB_HOST"),
+    "NAME": os.getenv("MDB_NAME", "testmongo"),
+    "HOST": os.getenv("MDB_HOST", "localhost"),
     "PORT": int(os.getenv("MDB_PORT", 27017)),
-    "USER": os.getenv("MDB_USER"),
-    "PASSWORD": os.getenv("MDB_PASSWORD"),
+    "USER": os.getenv("MDB_USER", "mongouser"),
+    "PASSWORD": os.getenv("MDB_PASSWORD", "mongopass"),
 }
 
 
@@ -130,9 +130,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-REDIS_HOST = "localhost"
-REDIS_PORT = 6379
+REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
+REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
 REDIS_DB = 0
+
+CELERY_BROKER_URL = f"redis://{REDIS_HOST}:{REDIS_PORT}/0"
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
@@ -151,6 +153,3 @@ SWAGGER_SETTINGS = {
     }
 }
 SWAGGER_USE_COMPAT_RENDERERS = False
-
-
-CELERY_BROKER_URL = "redis://localhost:6380/0"
